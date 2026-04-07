9d64a77bf7fcc374e4a6ebdb0eceffe471d475e8
Daniel González Rodero <d.gonzalez@fourthline.com>
2026-04-07 10:24:37 +0200

    fix(infra): use native S3 lockfile and update tf provider to v6

--- infra/bootstrap/backend.tf
+++ infra/bootstrap/backend.tf
@@ -1,16 +1,16 @@
 terraform {
   backend "s3" {
-    bucket         = "chatbot-tf-state-543704476214"
-    key            = "bootstrap/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "chatbot-tf-lock"
-    encrypt        = true
+    bucket       = "chatbot-tf-state-543704476214"
+    key          = "bootstrap/terraform.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
+    encrypt      = true
   }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 }
