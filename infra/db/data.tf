b32fcf1123b0a24cb1b13c98cfe2878c7f8bb6d5
Daniel González Rodero <d.gonzalez@fourthline.com>
2026-04-23 14:08:41 +0200

    add compute data to db

--- infra/db/data.tf
+++ infra/db/data.tf
@@ -1,9 +1,17 @@
-# data.tf
 data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
     bucket = "chatbot-tf-state-543704476214"
     key    = "network/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "compute" {
+  backend = "s3"
+  config = {
+    bucket = "chatbot-tf-state-543704476214" # El mismo bucket que usas en network
+    key    = "compute/terraform.tfstate"     # La ruta al estado de compute
     region = "eu-west-1"
   }
 }
