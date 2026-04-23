08e0e737f7d9b95ce0a59a60e074b371cce26d39
Daniel González Rodero <d.gonzalez@fourthline.com>
2026-04-23 18:32:38 +0200

    fix

--- infra/bootstrap/variables.tf
+++ infra/bootstrap/variables.tf
@@ -3,6 +3,11 @@ variable "aws_region" {
   type        = string
 }
 
+variable "aws_account_id" {
+  description = "ID de la cuenta de AWS"
+  type        = string
+}
+
 variable "project_name" {
   description = "Name of the project"
   type        = string
