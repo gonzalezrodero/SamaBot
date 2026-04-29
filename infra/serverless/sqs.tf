214b3a99340cdaf63783089da39bf1c669533543
Daniel González Rodero <d.gonzalez@fourthline.com>
2026-04-29 21:52:53 +0200

    fix

--- infra/serverless/sqs.tf
+++ infra/serverless/sqs.tf
@@ -1,4 +1,13 @@
+resource "aws_sqs_queue" "bot_dlq" {
+  name = "${var.project_name}-messages-dlq"
+}
+
 resource "aws_sqs_queue" "bot_queue" {
-  name                       = "chatbot-messages-queue"
+  name                       = "${var.project_name}-messages-queue"
   visibility_timeout_seconds = 60
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.bot_dlq.arn
+    maxReceiveCount     = 3
+  })
 }
